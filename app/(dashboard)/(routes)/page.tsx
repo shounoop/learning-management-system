f06dc95c75ae241df45da037577bb068f007e8f0
Shounoop <hmtuan9a@gmail.com>
2023-10-07 17:14:41 +0700

    authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-	return <div className="p-4">This is protected page</div>;
+	return (
+		<div>
+			<UserButton afterSignOutUrl="/" />
+		</div>
+	);
 }
---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+	return <div>This should be unprotected</div>;
+};
+
+export default TestPage;
