e54672b488a35df52eb851e08bacd90bb6d01e60
Shounoop <hmtuan9a@gmail.com>
2023-10-15 18:30:42 +0700

    course price form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,12 +1,13 @@
 import { IconBadge } from '@/components/icon-badge';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
-import { LayoutDashboard } from 'lucide-react';
+import { CircleDollarSign, LayoutDashboard, ListChecks } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import TitleForm from './_components/title-form';
 import DescriptionForm from './_components/description-form';
 import ImageForm from './_components/image-form';
 import CategoryForm from './_components/category-form';
+import PriceForm from './_components/price-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -69,7 +70,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 					</div>
 
 					<TitleForm initialData={course} courseId={course.id} />
-					
+
 					<DescriptionForm initialData={course} courseId={course.id} />
 
 					<ImageForm initialData={course} courseId={course.id} />
@@ -82,6 +83,28 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 							value: category.id,
 						}))}
 					/>
+				</div>
+
+				<div className="space-y-6">
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={ListChecks} />
+
+							<h2 className="text-xl">Course chapters</h2>
+						</div>
+
+						<div>TODO: Chapters</div>
+					</div>
+
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={CircleDollarSign} />
+
+							<h2 className="text-xl">Sell your course</h2>
+						</div>
+
+						<PriceForm initialData={course} courseId={course.id} />
+					</div>
 				</div>
 			</div>
 		</div>
