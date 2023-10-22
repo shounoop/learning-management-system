e742eff915798a68497a308b2fcb974245897524
Shounoop <hmtuan9a@gmail.com>
2023-10-22 13:47:54 +0700

    added confetti provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ToastProvider } from '@/components/providers/toast-provider';
+import ConfettiProvider from '@/components/providers/confetti-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body className={inter.className}>
+					<ConfettiProvider />
+
 					<ToastProvider />
+
 					{children}
 				</body>
 			</html>
---
+++ components/providers/confetti-provider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import ReactConfetti from 'react-confetti';
+import { useConfettiStore } from '@/hooks/use-confetti-store';
+
+const ConfettiProvider = () => {
+	const confetti = useConfettiStore();
+
+	if (!confetti.isOpen) return null;
+
+	return (
+		<ReactConfetti
+			className="pointer-events-none z-[100]"
+			numberOfPieces={500}
+			recycle={false}
+			onConfettiComplete={() => {
+				confetti.onClose();
+			}}
+		/>
+	);
+};
+
+export default ConfettiProvider;
