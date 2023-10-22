636e30fefab2aee5f5d87918d505fb1cda2bfb81
Shounoop <hmtuan9a@gmail.com>
2023-10-22 17:05:00 +0700

    categories query

---
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { IconType } from 'react-icons';
+import qs from 'query-string';
+
+interface CategoryItemProps {
+	label: string;
+	icon?: IconType;
+	value?: string;
+}
+
+const CategoryItem = ({ label, icon: Icon, value }: CategoryItemProps) => {
+	const pathname = usePathname();
+	const router = useRouter();
+	const searchParams = useSearchParams();
+
+	const currentCategoryId = searchParams.get('categoryId');
+	const currentTitle = searchParams.get('title');
+
+	const isSelected = currentCategoryId === value;
+
+	const onClick = () => {
+		const url = qs.stringifyUrl(
+			{
+				url: pathname,
+				query: {
+					title: currentTitle,
+					categoryId: isSelected ? null : value,
+				},
+			},
+			{
+				skipNull: true,
+				skipEmptyString: true,
+			}
+		);
+
+		router.push(url);
+	};
+
+	return (
+		<button
+			onClick={onClick}
+			type="button"
+			className={cn(
+				'py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition',
+				isSelected && 'border-sky-700 bg-sky-200/20 text-sky-800'
+			)}
+		>
+			{Icon && <Icon size={20} />}
+
+			<div className="truncate">{label}</div>
+		</button>
+	);
+};
+
+export default CategoryItem;
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,18 @@
-const SearchPage = () => {
-	return <div>Search Page</div>;
+import { db } from '@/lib/db';
+import Categories from './_components/categories';
+
+const SearchPage = async () => {
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: 'asc',
+		},
+	});
+
+	return (
+		<div className="p-6">
+			<Categories items={categories} />
+		</div>
+	);
 };
 
 export default SearchPage;
