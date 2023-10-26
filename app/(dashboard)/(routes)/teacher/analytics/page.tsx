41b0a6a33aace61e261af9829d707f3c312d73c9
Shounoop <hmtuan9a@gmail.com>
2023-10-26 18:17:58 +0700

    teacher analytics dashboard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,27 @@
-const AnalyticsPage = () => {
-	return <div>AnalyticsPage</div>;
+import { getAnalytics } from '@/actions/get-analytics';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import DataCard from './_components/data-card';
+import { Chart } from './_components/chart';
+
+const AnalyticsPage = async () => {
+	const { userId } = auth();
+
+	if (!userId) return redirect('/');
+
+	const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+	return (
+		<div className="p-6">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+				<DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+
+				<DataCard label="Total Sales" value={totalSales} />
+			</div>
+
+			{data.length > 0 && <Chart data={data} />}
+		</div>
+	);
 };
 
 export default AnalyticsPage;
