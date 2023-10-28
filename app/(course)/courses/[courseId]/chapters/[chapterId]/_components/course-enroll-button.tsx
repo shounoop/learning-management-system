9a73b830fca967c9d6d369e9f4a0bce827034634
Shounoop <hmtuan9a@gmail.com>
2023-10-28 23:27:45 +0700

    update purchase

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from '@/components/ui/button';
 import { formatPrice } from '@/lib/format';
+import axios from 'axios';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 interface CourseEnrollButtonProps {
 	courseId: string;
@@ -9,8 +12,30 @@ interface CourseEnrollButtonProps {
 }
 
 const CourseEnrollButton = ({ courseId, price }: CourseEnrollButtonProps) => {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const onClick = async () => {
+		try {
+			setIsLoading(true);
+
+			const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+			// redirect to the checkout page
+			window.location.assign(response.data.url);
+		} catch (error) {
+			toast.error('Something went wrong');
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
 	return (
-		<Button size={'sm'} className="w-full md:w-auto">
+		<Button
+			onClick={onClick}
+			disabled={isLoading}
+			size={'sm'}
+			className="w-full md:w-auto"
+		>
 			Enroll for {formatPrice(price)}
 		</Button>
 	);
